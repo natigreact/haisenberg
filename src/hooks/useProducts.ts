f87874a6efe97b37a2d5675eff39b7b91f80e43e
Natig <natikibishov@gmail.com>
2022-08-01 13:53:51 +0400

    Deleted folder idea from git

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,33 @@
+import {useEffect, useState} from "react";
+import {IProduct} from "../types/types";
+import axios, {AxiosError} from "axios";
+
+export const useProducts = () => {
+    const [products, setProducts] = useState<IProduct[]>([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    const addProduct = (product: IProduct) => {
+        setProducts(prev => [...prev, product])
+    }
+
+    const fetchProducts = async () => {
+        try {
+            setError('')
+            setLoading(true)
+            const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+            setProducts(response.data)
+            setLoading(false)
+        } catch (e: unknown) {
+            const error = e as AxiosError
+            setLoading(false)
+            setError(error.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, []);
+
+    return {products, loading, error , addProduct}
+}
---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const FavouritesPage = () => {
+    return (
+        <div className='container mx-auto max-w-2xl pt-5'>
+asdasd
+        </div>
+    );
+};
